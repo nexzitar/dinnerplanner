f396c6b5d29fc1141cd3a12abc997ee40cc087d7
nexzitar <nexzitar@gmail.com>
2020-04-19 14:10:21 +0200

    The first commit, with a working authentication backend.

---
+++ routes/auth.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const User = require('../model/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { registerValidation, loginValidation } = require('../validation');
+
+// VALIDATION
+
+router.post('/register', async (req, res) => {
+  // LETS VALIDATE THE DATA BEFORE WE MAKE A USER
+  // const validation = Joi.validate(req.body, schema);
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Checking if the user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send('Email already exists');
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // Create a new user
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.send({ user: user.id });
+  } catch {
+    res.status(400).send(err);
+  }
+});
+
+// LOGIN
+router.post('/login', async (req, res) => {
+  // LETS VALIDATE THE DATA BEFORE WE LOGIN A USER
+  // const validation = Joi.validate(req.body, schema);
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  // Checking if the user is already in the database
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('Email or password is wrong');
+  // PASSWORD IS CORRECT
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send('Email or password is wrong');
+
+  // Create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header('auth-token', token).send(token);
+});
+
+module.exports = router;
